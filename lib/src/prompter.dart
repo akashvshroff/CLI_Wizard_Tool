9d4bc4a72796be4b6790de6627c10c43d457f86a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-11-17 13:47:18 +0530

    Prompter class

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -1,1 +1,14 @@
-class Prompter {}
+import 'terminal.dart';
+import 'option.dart';
+
+final Terminal terminal = Terminal();
+
+class Prompter {
+  Option ask(String prompt, List<Option> options) {
+    terminal.clearScreen();
+    terminal.printPrompt(prompt);
+    terminal.printOptions(options);
+    String response = terminal.collectInput();
+    print(response);
+  }
+}
