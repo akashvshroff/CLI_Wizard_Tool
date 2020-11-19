d6d94ecffbe93b66cf311f760f211f137be5fc12
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-11-19 13:48:44 +0530

    Getting input and returning

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,11 +4,11 @@ import 'option.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  Option ask(String prompt, List<Option> options) {
+  dynamic ask(String prompt, List<Option> options) {
     _terminal.clearScreen();
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
-    String response = _terminal.collectInput();
-    print(response);
+    final String input = _terminal.collectInput();
+    return options[int.parse(input)].value;
   }
 }
