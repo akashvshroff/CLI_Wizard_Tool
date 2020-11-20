279b2a3a2d88a2dee96b28705cf1778d13625ef0
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-11-20 10:10:36 +0530

    Example file

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,18 @@
+import 'package:cli_wizard_avs/cli_wizard_avs.dart';
+
 void main() {
-  print("Hi there");
+  List<Option> options = [
+    Option(label: 'I want red', value: '#f00'),
+    Option(label: 'I want blue', value: '#00f'),
+    Option(label: 'I want white', value: '#fff'),
+    Option(label: 'I want black', value: '#000'),
+  ];
+  final prompter = Prompter();
+
+  String colourCode = prompter.askMultiple('Select a colour', options);
+
+  bool answer = prompter.askBinary('Do you like this lib?');
+
+  print('Colour code selected: $colourCode');
+  print('Answer entered: $answer');
 }
